b10ee9b2ae03cb6661e91942a634a9bd45cce32d
leopiccionia <leopiccionia@gmail.com>
2017-06-03 13:07:48 -0300

    Inserting values into MongoDB.

--- licitasp/pipelines.py
+++ licitasp/pipelines.py
@@ -5,7 +5,24 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import datetime
+import pymongo
 
-class LicitaspPipeline(object):
+class AcquisitionsPipeline(object):
+
+    def __init__(self):
+        self.client = None
+        self.base = None
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient()
+        self.base = self.client['licitasp']
+
     def process_item(self, item, spider):
+        dict_item = dict(item)
+        dict_item['timestamp'] = datetime.datetime.utcnow()
+        self.client['acquisitions'].insert(dict_item)
         return item
+
+    def close_spider(self, spider):
+        self.client.close()
--- licitasp/settings.py
+++ licitasp/settings.py
@@ -63,9 +63,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'licitasp.pipelines.LicitaspPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'licitasp.pipelines.AcquisitionsPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
